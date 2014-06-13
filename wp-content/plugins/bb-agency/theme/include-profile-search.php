76b4abc74648646a9ec8cc21f7b697e6cf7624e8
Paul Jenkins <paul@turnpiece.com>
2014-06-13 22:13:32 +0100

    restructuring front end search code

--- wp-content/plugins/bb-agency/theme/include-profile-search.php
+++ wp-content/plugins/bb-agency/theme/include-profile-search.php
@@ -1,140 +1,150 @@
 <?php
-	global $wpdb, $bb_agency_CURRENT_TYPE_ID;
-	$bb_options = bb_agency_get_option();
-	$bb_agency_option_profilenaming = bb_agency_get_option('bb_agency_option_profilenaming');
-	$bb_agency_option_unittype = bb_agency_get_option('bb_agency_option_unittype');
+global $wpdb, $bb_agency_CURRENT_TYPE_ID;
 
-	//Custom fields display options
-	$bb_agency_option_customfields_profilepage = bb_agency_get_option('bb_agency_option_customfield_profilepage');
-	$bb_agency_option_customfields_searchpage = bb_agency_get_option('bb_agency_option_customfield_searchpage');
-	$bb_agency_option_customfields_loggedin_all = bb_agency_get_option('bb_agency_option_customfield_loggedin_all');
-	$bb_agency_option_customfields_loggedin_admin = bb_agency_get_option('bb_agency_option_customfield_loggedin_admin');
+$bb_agency_option_profilenaming = bb_agency_get_option('bb_agency_option_profilenaming');
+$bb_agency_option_unittype = bb_agency_get_option('bb_agency_option_unittype');
+
+// Custom fields display options
+$bb_agency_option_customfields_profilepage = bb_agency_get_option('bb_agency_option_customfield_profilepage');
+$bb_agency_option_customfields_searchpage = bb_agency_get_option('bb_agency_option_customfield_searchpage');
+$bb_agency_option_customfields_loggedin_all = bb_agency_get_option('bb_agency_option_customfield_loggedin_all');
+$bb_agency_option_customfields_loggedin_admin = bb_agency_get_option('bb_agency_option_customfield_loggedin_admin');
+
+if (!empty($bb_agency_CURRENT_TYPE_ID)) 
+	$_SESSION['ProfileType'] = $bb_agency_CURRENT_TYPE_ID;
+
+if (isset($_REQUEST['t']) && !empty($_REQUEST['t'])) { 
+	$_SESSION['ProfileType'] = $_REQUEST['t']; 
+}
+if (isset($DataTypeID) && !empty($DataTypeID)) { 
+	$_SESSION['ProfileType'] = $DataTypeID; 
+}
+if (isset($_REQUEST['g']) && !empty($_REQUEST['g'])) {  
+	$_SESSION['ProfileGender'] = $_REQUEST['g']; 
+}
+
+// fix advanced search to include
+// custom fields from search fields
+if (isset($_GET['srch'])){
+   $profilesearch_layout = "advanced"; 	
+}
+
+if ($profilesearch_layout == "condensed" || $profilesearch_layout == "simple") : ?>
+<div id="profile-search-form-condensed" class="rbsearch-form">
+    <form method="post" id="search-form-condensed" action="<?php echo get_bloginfo("wpurl") ?>/profile-search/">
+        <input type="hidden" name="action" value="search" />
+	 	<div class="search-field single">
+ 			<label for="ProfileFirstName"><?php _e("First Name", bb_agency_TEXTDOMAIN) ?></label>
+ 			<input type="text" id="ProfileContactNameFirst" name="cnf" value="<?php echo $_SESSION["ProfileContactNameFirst"] ?>" />
+	 	</div>		
+		<div class="search-field single">
+		    <label for="ProfileType"><?php _e("Type", bb_agency_TEXTDOMAIN) ?></label>
+		    <select name="ProfileType" id="ProfileType">\              
+				<option value=""><?php _e("Any Profile Type", bb_agency_TEXTDOMAIN) ?></option>
+				<?php
+					$query = "SELECT DataTypeID, DataTypeTitle FROM ". table_agency_data_type ." WHERE DataTypeID <> ".bb_agency_CLIENTS_ID." ORDER BY DataTypeTitle DESC";
+					$results2 = mysql_query($query);
+					while ($dataType = mysql_fetch_array($results2)) :
+						if ($_SESSION['ProfileType']) {
+							if ($dataType["DataTypeID"] ==  $_SESSION['ProfileType']) { 
+								$selectedvalue = " selected"; 
+							} else { 
+								$selectedvalue = ""; 
+							} 
+						} else { 
+							$selectedvalue = ""; 
+						}
+						?>
+				<option value="<?php echo $dataType["DataTypeID"] ?>"<?php echo $selectedvalue ?>><?php echo $dataType["DataTypeTitle"] ?></option>
+					<?php endwhile; ?>
+		    </select>
+		</div>               
+		<div class="search-field submit">
+			<input type="submit" value="<?php _e("Search Profiles", bb_agency_TEXTDOMAIN) ?>" class="button-primary" onclick="this.form.action='<?php echo get_bloginfo("wpurl") ?>/profile-search/'" />
 	
-	if (!empty($bb_agency_CURRENT_TYPE_ID)) $_SESSION['ProfileType'] = $bb_agency_CURRENT_TYPE_ID;
-	if (isset($_REQUEST['ProfileType']) && !empty($_REQUEST['ProfileType'])) { $_SESSION['ProfileType'] = $_REQUEST['ProfileType']; }
-	if (isset($DataTypeID) && !empty($DataTypeID)) { $_SESSION['ProfileType'] = $DataTypeID; }
-	if (isset($_REQUEST['ProfileGender']) && !empty($_REQUEST['ProfileGender'])) {  $_SESSION['ProfileGender'] = $_REQUEST['ProfileGender']; }
-	
-	// fix advanced search to include
-	// custom fields from search fields
-	if (isset($_GET['srch'])){
-	   $profilesearch_layout = "advanced"; 	
+			<input type="submit" name="advanced_search" value="<?php _e("Advanced Search", bb_agency_TEXTDOMAIN) ?>" class="button-primary" onclick="this.form.action='<?php echo get_bloginfo("wpurl") ?>/search/?srch=1'" />
+		</div>
+    </form>
+</div>
+<?php else : // Advanced ?>
+   
+ <form method="post" id="search-form-advanced" action="". get_bloginfo("wpurl") ."/profile-search/" class="rbsearch-form">
+	<input type="hidden" name="page" id="page" value="bb_agency_search" />
+	<input type="hidden" name="action" value="search" />
+	<div class="search-field single">
+		<label for="cnf"><?php _e("First Name", bb_agency_TEXTDOMAIN) ?></label>
+		<input type="text" id="ProfileContactNameFirst" name="cnf" value="<?php echo $_SESSION["ProfileContactNameFirst"] ?>" />
+	</div>
+    <div class="search-field single">
+       <label for="t"><?php _e("Type", bb_agency_TEXTDOMAIN) ?></label>\
+		<select name="t" id="ProfileType">\               
+			<option value=""><?php _e("Any Profile Type", bb_agency_TEXTDOMAIN) ?></option>
+			<?php
+				$query = "SELECT DataTypeID, DataTypeTitle FROM ". table_agency_data_type ." WHERE DataTypeID <> ".bb_agency_CLIENTS_ID." ORDER BY DataTypeTitle DESC";
+				$results2 = mysql_query($query);
+				while ($dataType = mysql_fetch_array($results2)) : ?>
+			<option value="<?php echo $dataType["DataTypeID"] ?>" <?php selected($_SESSION['ProfileType'], $dataType["DataTypeID"], false) ?>><?php echo $dataType["DataTypeTitle"] ?></option>
+				<?php endwhile; ?>
+        </select>
+    </div>
+
+    <fieldset class="search-field multi">
+        <legend><?php _e("Date of birth", bb_agency_TEXTDOMAIN) ?> (* <?php _e("applies to baby searches only", bb_agency_TEXTDOMAIN) ?>)</legend>
+
+	    <div>
+	        <label for="dob_min"><?php _e("From", bb_agency_TEXTDOMAIN) ?></label>
+	        <input type="text" class="stubby bbdatepicker" id="ProfileDateBirth_min" name="dob_min" value="<?php echo $_SESSION['ProfileDateBirth_min'] ?>" />
+	    </div>
+	    <div>
+	    	<label for="dob_max"><?php _e("To", bb_agency_TEXTDOMAIN) ?></label>\
+	        <input type="text" class="stubby bbdatepicker" id="ProfileDateBirth_max" name="dob_max" value="<?php echo $_SESSION['ProfileDateBirth_max'] ?>" />
+	    </div>
+	</fieldset>
+
+	<fieldset class="search-field multi">
+	    <legend><?php _e("Due date", bb_agency_TEXTDOMAIN) ?></legend>
+	    <div>
+	        <label for="dd_min"><?php _e("From", bb_agency_TEXTDOMAIN) ?></label>
+	        <input type="text" class="stubby bbdatepicker" id="ProfileDateDue_min" name="dd_min" value="<?php echo $_SESSION['ProfileDateDue_min'] ?>" />
+	    </div>
+	    <div>
+	    	<label for="dd_max"><?php _e("To", bb_agency_TEXTDOMAIN) ?></label>\
+	        <input type="text" class="stubby bbdatepicker" id="ProfileDateDue_max" name="dd_max" value="<?php echo $_SESSION['ProfileDateDue_max'] ?>" />
+	    </div>
+	</fieldset>							
+	<?php	
+	if ($bb_agency_option_customfields_searchpage == 1 || $bb_agency_option_customfield_profilepage == 1 OR $_POST['advanced_search']) { // Show on Search Page or Profile Page
+
+		if (is_user_logged_in()) { // All with loggedin permissions
+
+		} else { // All with non-logged here
+		
+			// Show custom fields to public
+	    	if($_REQUEST["action"] != 'search' || $_REQUEST["action"] ==''){
+		    	//include("include-custom-fields.php");
+				$profilesearch_layout = "";
+			}
+		}
 	}
-	
-   	if ($profilesearch_layout == "condensed" || $profilesearch_layout == "simple") {
-	
-		echo "		<div id=\"profile-search-form-condensed\" class=\"rbsearch-form\">\n";
-		echo "        	<form method=\"post\" id=\"search-form-condensed\" action=\"". get_bloginfo("wpurl") ."/profile-search/\">\n";
-		echo "        		<input type=\"hidden\" name=\"action\" value=\"search\" />\n";
-		echo "	 			    <div class=\"search-field single\">\n";
-		echo "		 				<label for=\"ProfileFirstName\">". __("First Name", bb_agency_TEXTDOMAIN) ."</label>\n";
-	    echo "		 				<input type=\"text\" id=\"ProfileContactNameFirst\" name=\"ProfileContactNameFirst\" value=\"". $_SESSION["ProfileContactNameFirst"] ."\" />\n";
-	    echo "	 				</div>\n";		
-		echo "				    <div class=\"search-field single\">\n";
-		echo "				       <label for=\"ProfileType\">". __("Type", bb_agency_TEXTDOMAIN) . "</label>\n";
-		echo "				        	<select name=\"ProfileType\" id=\"ProfileType\">\n";               
-		echo "								<option value=\"\">". __("Any Profile Type", bb_agency_TEXTDOMAIN) . "</option>";
-											$query = "SELECT DataTypeID, DataTypeTitle FROM ". table_agency_data_type ." WHERE DataTypeID <> ".bb_agency_CLIENTS_ID." ORDER BY DataTypeTitle DESC";
-											$results2 = mysql_query($query);
-											while ($dataType = mysql_fetch_array($results2)) {
-												if ($_SESSION['ProfileType']) {
-													if ($dataType["DataTypeID"] ==  $_SESSION['ProfileType']) { 
-														$selectedvalue = " selected"; 
-													} else { 
-														$selectedvalue = ""; 
-													} 
-												} else { 
-													$selectedvalue = ""; 
-												}
-												echo "<option value=\"". $dataType["DataTypeID"] ."\"".$selectedvalue.">". $dataType["DataTypeTitle"] ."</option>";
-											}
-		echo "				        	</select>\n";
-		echo "				    </div>\n";                
-		echo "				<div class=\"search-field submit\">";
-		echo "				<input type=\"submit\" value=\"". __("Search Profiles", bb_agency_TEXTDOMAIN) . "\" class=\"button-primary\" onclick=\"this.form.action='".get_bloginfo("wpurl")."/profile-search/'\" />";
-		
-		echo "				<input type=\"submit\" name=\"advanced_search\" value=\"". __("Advanced Search", bb_agency_TEXTDOMAIN) . "\" class=\"button-primary\" onclick=\"this.form.action='".get_bloginfo("wpurl")."/search/?srch=1'\" />";
-		
-		echo "				</div>\n";
-		echo "        	</form>\n";
-		echo "		</div>\n";
-   } else {
-	   // Advanced
-		echo "  <form method=\"post\" id=\"search-form-advanced\" action=\"". get_bloginfo("wpurl") ."/profile-search/\" class=\"rbsearch-form\">\n";
-		echo "        		<input type=\"hidden\" name=\"page\" id=\"page\" value=\"bb_agency_search\" />\n";
-		echo "        		<input type=\"hidden\" name=\"action\" value=\"search\" />\n";
-		
-		echo "	 			      <div class=\"search-field single\">\n";
-		echo "		 				<label for=\"ProfileFirstName\">". __("First Name", bb_agency_TEXTDOMAIN) ."</label>\n";
-	      echo "		 				<input type=\"text\" id=\"ProfileContactNameFirst\" name=\"ProfileContactNameFirst\" value=\"". $_SESSION["ProfileContactNameFirst"] ."\" />\n";
-	      echo "	 				</div>\n";
-
-		echo "				    <div class=\"search-field single\">\n";
-		echo "				       <label for=\"ProfileType\">". __("Type", bb_agency_TEXTDOMAIN) . "</label>\n";
-		echo "						<select name=\"ProfileType\" id=\"ProfileType\">\n";               
-		echo "								<option value=\"\">". __("Any Profile Type", bb_agency_TEXTDOMAIN) . "</option>";
-											$query = "SELECT DataTypeID, DataTypeTitle FROM ". table_agency_data_type ." WHERE DataTypeID <> ".bb_agency_CLIENTS_ID." ORDER BY DataTypeTitle DESC";
-											$results2 = mysql_query($query);
-											while ($dataType = mysql_fetch_array($results2)) {
-												echo "<option value=\"". $dataType["DataTypeID"] ."\"".selected($_SESSION['ProfileType'],$dataType["DataTypeID"] ,false).">". $dataType["DataTypeTitle"] ."</option>";
-											}
-		echo "				        	</select>";
-		echo "				    </div>\n";
-
-		echo "				    <fieldset class=\"search-field multi\">";
-		echo "				        <legend>". __("Date of birth", bb_agency_TEXTDOMAIN) . " (*" . __("applies to baby searches only", bb_agency_TEXTDOMAIN). ")</legend>";
-		
-		echo "				    <div>";
-		echo "				        <label for=\"ProfileDateBirth_min\">". __("From", bb_agency_TEXTDOMAIN) . "</label>";
-		echo "				        	<input type=\"text\" class=\"stubby bbdatepicker\" id=\"ProfileDateBirth_min\" name=\"ProfileDateBirth_min\" value=\"". $_SESSION['ProfileDateBirth_min'] ."\" /></div>";
-		echo "				        <div><label for=\"ProfileDateBirth_max\">". __("To", bb_agency_TEXTDOMAIN) . "</label>\n";
-		echo "				        	<input type=\"text\" class=\"stubby bbdatepicker\" id=\"ProfileDateBirth_max\" name=\"ProfileDateBirth_max\" value=\"". $_SESSION['ProfileDateBirth_max'] ."\" /></div>";
-		echo "				    </fieldset>";
-
-		echo "				    <fieldset class=\"search-field multi\">";
-		echo "				        <legend>". __("Due date", bb_agency_TEXTDOMAIN) . "</legend>";
-		
-		echo "				    <div>";
-		echo "				        <label for=\"ProfileDateDue_min\">". __("From", bb_agency_TEXTDOMAIN) . "</label>";
-		echo "				        	<input type=\"text\" class=\"stubby bbdatepicker\" id=\"ProfileDateDue_min\" name=\"ProfileDateDue_min\" value=\"". $_SESSION['ProfileDateDue_min'] ."\" /></div>";
-		echo "				        <div><label for=\"ProfileDateDue_max\">". __("To", bb_agency_TEXTDOMAIN) . "</label>\n";
-		echo "				        	<input type=\"text\" class=\"stubby bbdatepicker\" id=\"ProfileDateDue_max\" name=\"ProfileDateDue_max\" value=\"". $_SESSION['ProfileDateDue_max'] ."\" /></div>";
-		echo "				    </fieldset>";							
-			
-		if($bb_agency_option_customfields_searchpage == 1 || $bb_agency_option_customfield_profilepage == 1 OR $_POST['advanced_search']){ // Show on Search Page or Profile Page
-
-			if (is_user_logged_in()) { // All with loggedin permissions
-
-
-			} else { // All with non-logged here
-				
-				// Show custom fields to public
-			    if($_REQUEST["action"] != 'search' || $_REQUEST["action"] ==''){
-				    	//include("include-custom-fields.php");
-						$profilesearch_layout = "";
-					}
-				}
-			
-		}
-		
-		if (isset($_GET['srch'])) {
-			include("include-custom-fields.php");
-		}
-		
-		echo "				<div class=\"search-field submit\">";
-		echo "				<input type=\"submit\" value=\"". __("Search Profiles", bb_agency_TEXTDOMAIN) . "\" class=\"button-primary\" onclick=\"this.form.action='".get_bloginfo("wpurl")."/profile-search/'\" />";
-		echo "				<input type=\"reset\" class=\"button-primary\" value=\"". __("Empty Form", bb_agency_TEXTDOMAIN) . "\">";
-		if(!isset($_GET[srch])){
-			echo "				<input type=\"submit\" name=\"advanced_search\" value=\"". __("Advanced Search", bb_agency_TEXTDOMAIN) . "\" class=\"button-primary\" onclick=\"this.form.action='".get_bloginfo("wpurl")."/search/?srch=1'\" />";}else{
-		
-			echo "				<input type=\"submit\" name=\"basic_search\" value=\"". __("Basic Search", bb_agency_TEXTDOMAIN) . "\" class=\"button-primary\" onclick=\"this.form.action='".get_bloginfo("wpurl")."/search'\" />";
-		}
-
-		echo "				</div>";
-		if(isset($_GET['srch'])){ echo'<div></div>'; $style="style='margin-left:0px'"; }else{$style="";}
-		
-		echo "        	</form>\n";
-		
-	    	
-   }
-?>
+	if (isset($_GET['srch'])) {
+		include("include-custom-fields.php");
+	}
+	?>
+	<div class="search-field submit">
+		<input type="submit" value="<?php _e("Search Profiles", bb_agency_TEXTDOMAIN) ?>" class="button-primary" onclick="this.form.action='<?php echo get_bloginfo("wpurl") ?>/profile-search/'" />
+		<input type="reset" class="button-primary" value="<?php _e("Empty Form", bb_agency_TEXTDOMAIN) ?>">
+		<?php if (!isset($_GET[srch])) : ?>
+		<input type="submit" name="advanced_search" value="<?php _e("Advanced Search", bb_agency_TEXTDOMAIN) ?>" class="button-primary" onclick="this.form.action='<?php echo get_bloginfo("wpurl") ?>/search/?srch=1'" />
+		<?php else : ?>
+		<input type="submit" name="basic_search" value="<?php _e("Basic Search", bb_agency_TEXTDOMAIN) ?>" class="button-primary" onclick="this.form.action='<?php echo get_bloginfo("wpurl") ?>/search'" />
+		<?php endif; ?>
+	</div>
+	<?php if (isset($_GET['srch'])) { 
+		echo'<div></div>'; 
+		$style="style='margin-left:0px'"; 
+	} else {
+		$style="";
+	} ?>
+</form>
+    	
+<?php endif; ?>
--- wp-content/plugins/bb-agency/theme/view-search.php
+++ wp-content/plugins/bb-agency/theme/view-search.php
@@ -17,7 +17,7 @@ if ($_REQUEST["action"] == "search") {
 		
 	// Sort By
 	$sort = "";
-	if (isset($_REQUEST['sort']) && !empty($_REQUEST['sort'])){
+	if (isset($_REQUEST['sort']) && !empty($_REQUEST['sort'])) {
 		$sort = $_REQUEST['sort'];
 	}
 	else {
@@ -25,7 +25,7 @@ if ($_REQUEST["action"] == "search") {
 	}
 
 	// Limit
-	if (isset($_REQUEST['limit']) && !empty($_REQUEST['limit'])){
+	if (isset($_REQUEST['limit']) && !empty($_REQUEST['limit'])) {
 	}
 	else {
 		$limit = " LIMIT 0,100";
@@ -33,9 +33,9 @@ if ($_REQUEST["action"] == "search") {
 
 	// Sort Order
 	$dir = "";
-	if (isset($_REQUEST['dir']) && !empty($_REQUEST['dir'])){
+	if (isset($_REQUEST['dir']) && !empty($_REQUEST['dir'])) {
 		$dir = $_REQUEST['dir'];
-		if ($dir == "desc" || !isset($dir) || empty($dir)){
+		if ($dir == "desc" || !isset($dir) || empty($dir)) {
 		   	$sortDirection = "asc";
 	   	} else {
 		   	$sortDirection = "desc";
@@ -49,94 +49,94 @@ if ($_REQUEST["action"] == "search") {
 	$filterArray = array();
 
 	// Name
-	if ((isset($_REQUEST['ProfileContactNameFirst']) && !empty($_REQUEST['ProfileContactNameFirst'])) || isset($_REQUEST['ProfileContactNameLast']) && !empty($_REQUEST['ProfileContactNameLast'])){
-	  	if (isset($_REQUEST['ProfileContactNameFirst']) && !empty($_REQUEST['ProfileContactNameFirst'])){
-			$filterArray['profilecontactnamefirst'] = $_REQUEST['ProfileContactNameFirst'];
+	if ((isset($_REQUEST['cnf']) && !empty($_REQUEST['cnf'])) || isset($_REQUEST['cnl']) && !empty($_REQUEST['cnl'])) {
+	  	if (isset($_REQUEST['cnf']) && !empty($_REQUEST['cnf'])) {
+			$filterArray['profilecontactnamefirst'] = $_REQUEST['cnf'];
 	  	}
-	  	if (isset($_REQUEST['ProfileContactNameLast']) && !empty($_REQUEST['ProfileContactNameLast'])){
-			$filterArray['profilecontactnamelast'] = $_REQUEST['ProfileContactNameLast'];
+	  	if (isset($_REQUEST['cnl']) && !empty($_REQUEST['cnl'])) {
+			$filterArray['profilecontactnamelast'] = $_REQUEST['cnl'];
 	  	}
 	}
 
 	// Location
-	if (isset($_REQUEST['ProfileLocationCity']) && !empty($_REQUEST['ProfileLocationCity'])){
+	if (isset($_REQUEST['ProfileLocationCity']) && !empty($_REQUEST['ProfileLocationCity'])) {
 		$filterArray['profilelocationcity'] = $_REQUEST['ProfileLocationCity'];
 	}
 
 	// Type
-	if (isset($_REQUEST['ProfileType']) && !empty($_REQUEST['ProfileType'])){
-		$filterArray['profiletype'] = $_REQUEST['ProfileType'];
+	if (isset($_REQUEST['t']) && !empty($_REQUEST['t'])) {
+		$filterArray['profiletype'] = $_REQUEST['t'];
 	}
 
 	// Gender
-	if (isset($_REQUEST['ProfileGender']) && !empty($_REQUEST['ProfileGender'])){
-		$filterArray['profilegender'] = $_REQUEST['ProfileGender'];
+	if (isset($_REQUEST['g']) && !empty($_REQUEST['g'])) {
+		$filterArray['profilegender'] = $_REQUEST['g'];
 	}
 	
 	// Height
-	if (isset($_REQUEST['ProfileStatHeight_min']) && !empty($_REQUEST['ProfileStatHeight_min'])){
-		$filterArray['profilestatheight_min'] = $_REQUEST['ProfileStatHeight_min'];
+	if (isset($_REQUEST['sh_min']) && !empty($_REQUEST['sh_min'])) {
+		$filterArray['profilestatheight_min'] = $_REQUEST['sh_min'];
 	}
-	if (isset($_REQUEST['ProfileStatHeight_max']) && !empty($_REQUEST['ProfileStatHeight_max'])){
-		$filterArray['profilestatheight_max'] = $_REQUEST['ProfileStatHeight_max'];
+	if (isset($_REQUEST['sh_max']) && !empty($_REQUEST['sh_max'])) {
+		$filterArray['profilestatheight_max'] = $_REQUEST['sh_max'];
 	}
 
 	// Weight
-	if (isset($_REQUEST['ProfileStatWeight_min']) && !empty($_REQUEST['ProfileStatWeight_min'])){
-		$filterArray['profilestatweight_min'] = $_REQUEST['ProfileStatWeight_min'];
+	if (isset($_REQUEST['sw_min']) && !empty($_REQUEST['sw_min'])) {
+		$filterArray['profilestatweight_min'] = $_REQUEST['sw_min'];
 	}
-	if (isset($_REQUEST['ProfileStatWeight_max']) && !empty($_REQUEST['ProfileStatWeight_max'])){
-		$filterArray['profilestatweight_max'] = $_REQUEST['ProfileStatWeight_max'];
+	if (isset($_REQUEST['sw_max']) && !empty($_REQUEST['sw_max'])) {
+		$filterArray['profilestatweight_max'] = $_REQUEST['sw_max'];
 	}
 
 	// Age
-	if (isset($_REQUEST['ProfileAge_min']) && !empty($_REQUEST['ProfileAge_min'])){
-		$filterArray['profileage_min'] = $_REQUEST['ProfileAge_min'];
+	if (isset($_REQUEST['a_min']) && !empty($_REQUEST['a_min'])) {
+		$filterArray['profileage_min'] = $_REQUEST['a_min'];
 	}
-	if (isset($_REQUEST['ProfileAge_max']) && !empty($_REQUEST['ProfileAge_max'])){
-		$filterArray['profileage_max'] = $_REQUEST['ProfileAge_max'];
+	if (isset($_REQUEST['a_max']) && !empty($_REQUEST['a_max'])) {
+		$filterArray['profileage_max'] = $_REQUEST['a_max'];
 	}
 
 	// Date of birth
-	if (isset($_REQUEST['ProfileDateBirth_min']) && !empty($_REQUEST['ProfileDateBirth_min'])){
-		$filterArray['profiledatebirth_min'] = $_REQUEST['ProfileDateBirth_min'];
+	if (isset($_REQUEST['dob_min']) && !empty($_REQUEST['dob_min'])) {
+		$filterArray['profiledatebirth_min'] = $_REQUEST['dob_min'];
 	}
-	if (isset($_REQUEST['ProfileDateBirth_max']) && !empty($_REQUEST['ProfileDateBirth_max'])){
-		$filterArray['profiledatebirth_max'] = $_REQUEST['ProfileDateBirth_max'];
+	if (isset($_REQUEST['dob_max']) && !empty($_REQUEST['dob_max'])) {
+		$filterArray['profiledatebirth_max'] = $_REQUEST['dob_max'];
 	}
 
 	// Due date
-	if (isset($_REQUEST['ProfileDateDue_min']) && !empty($_REQUEST['ProfileDateDue_min'])){
-		$filterArray['profiledatedue_min'] = $_REQUEST['ProfileDateDue_min'];
+	if (isset($_REQUEST['dd_min']) && !empty($_REQUEST['dd_min'])) {
+		$filterArray['profiledatedue_min'] = $_REQUEST['dd_min'];
 	}
-	if (isset($_REQUEST['ProfileDateDue_max']) && !empty($_REQUEST['ProfileDateDue_max'])){
-		$filterArray['profiledatedue_max'] = $_REQUEST['ProfileDateDue_max'];
+	if (isset($_REQUEST['dd_max']) && !empty($_REQUEST['dd_max'])) {
+		$filterArray['profiledatedue_max'] = $_REQUEST['dd_max'];
 	}
 
 	// City
-	if (isset($_REQUEST['ProfileCity']) && !empty($_REQUEST['ProfileCity'])){
-		$filterArray['profilecity'] = $_REQUEST['ProfileCity'];
+	if (isset($_REQUEST['c']) && !empty($_REQUEST['c'])) {
+		$filterArray['profilecity'] = $_REQUEST['c'];
 	}		
 
 	// State
-	if (isset($_REQUEST['ProfileState']) && !empty($_REQUEST['ProfileState'])){
-		$filterArray['profilestate'] = $_REQUEST['ProfileState'];
+	if (isset($_REQUEST['s']) && !empty($_REQUEST['s'])) {
+		$filterArray['profilestate'] = $_REQUEST['s'];
 	}		
 
 	// ZIP
-	if (isset($_REQUEST['ProfileZip']) && !empty($_REQUEST['ProfileZip'])){
-		$filterArray['profilezip'] = $_REQUEST['ProfileZip'];
+	if (isset($_REQUEST['z']) && !empty($_REQUEST['z'])) {
+		$filterArray['profilezip'] = $_REQUEST['z'];
 	}
 	
 	// Custom Fields
-	foreach($_POST as $key =>$val){
+	foreach ($_POST as $key =>$val) {
 	
-		if(substr($key,0,15)=="ProfileCustomID"){
-	      	if(is_array($val)){
-				if(count($val)>1){	
+		if (substr($key,0,15) == "ProfileCustomID") {
+	      	if (is_array($val)) {
+				if (count($val)>1) {	
 					$filterArray[$key] = implode(",",$val);	
 			 	} else {
-				 	if(!empty($val)){
+				 	if (!empty($val)) {
 						$filterArray[$key] = $val;
 					}
 			 	}
@@ -154,55 +154,30 @@ if ($_REQUEST["action"] == "search") {
 // *************************************************************************************************** //
 // GET HEADER 
 
-	get_header();
- /*  $sql = "SELECT * FROM ".table_agency_customfield_mux."";
-    $q = mysql_query($sql) or die(mysql_error());
-     while($f = mysql_fetch_assoc($q)){
-		// print_r($f);
-		 //echo "<br/>"; 
-	 }
- */
-	echo "<div id=\"primary\" class=\"eight column\">\n";
-	echo "    <div id=\"content\" role=\"main\" class=\"transparent\">\n";
-
-		echo "<div id=\"profile-search\">\n";
-	    
-			if ($_REQUEST["action"] == "search") {
-		echo "  <h1 class=\"entry-title\">". __("Search Results", bb_agency_TEXTDOMAIN) ."</h1>\n";
-			} else {
-		echo "  <h1 class=\"entry-title\">". __("Advanced Search", bb_agency_TEXTDOMAIN) ."</h1>\n";
-		//echo "  <h1 class=\"entry-title\">". __("Search Results", bb_agency_TEXTDOMAIN) ."</h1>\n";
-			}
-	
-		echo "  <div class=\"cb\"></div>\n";
-#DEBUG 
-#print_r($filterArray);
-		echo "		    <div id=\"profile-search-results\">\n";
-						if ($_REQUEST["action"] == "search") {
-	                                 
-							if (function_exists('bb_agency_profilelist')) { 
-								bb_agency_profilelist($filterArray); 
-							}
-						} else {
-							echo "<strong>". __("No search chriteria selected, please initiate your search.", bb_agency_TEXTDOMAIN) ."</strong>";
-						}
-
-/* Phel Comment: Removed Extra Search, We have already a Search widget
-		echo "		    </div>\n";
-		echo "		    <div id=\"profile-search-filter\" class=\"three column\">\n";
-		echo "			<h3 class=\"title\">". __("Search Profiles", bb_agency_TEXTDOMAIN) ."</h3>\n";
-		                        $isSearchPage = (get_query_var('type') == "search") ? 1 : 0;
-						$profilesearch_layout = "advanced";
-						include("include-profile-search.php"); 	
-		
-		echo "		    </div>\n";
-*/
-		echo "</div><!-- #profile-search-results -->\n"; // #profile-search-results
-		echo "</div><!-- #profile-search -->\n"; // #profile-search
-		echo "</div><!-- #content -->\n"; // #content
-
-	echo "  </div><!-- #primary -->\n"; // #primary
-
-//get_sidebar(); 
-get_footer(); 
-?>
+get_header(); ?>
+<div id="primary" class="eight column">
+    <div id="content" role="main" class="transparent">
+		<div id="profile-search">
+			<h1 class="entry-title">
+			<?php if ($_REQUEST["action"] == "search") : ?>
+		  	<?php _e("Search Results", bb_agency_TEXTDOMAIN) ?>
+			<?php else : ?>
+		 	<?php _e("Advanced Search", bb_agency_TEXTDOMAIN) ?>
+			<?php endif; ?>
+			</h1>
+	  		<div class="cb"></div>
+			<div id="profile-search-results">
+				<?php if ($_REQUEST["action"] == "search") :
+                     
+				if (function_exists('bb_agency_profilelist')) { 
+					bb_agency_profilelist($filterArray); 
+				}
+				?>
+				<?php else : ?>
+				<strong><?php _e("No search criteria selected.", bb_agency_TEXTDOMAIN) ?></strong>
+				<?php endif; ?>
+			</div><!-- #profile-search-results -->
+		</div><!-- #profile-search -->
+	</div><!-- #content -->
+</div><!-- #primary -->
+<?php get_footer() ?>
