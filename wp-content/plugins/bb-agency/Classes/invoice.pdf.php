4b677f1ca862e76b6ad52d3f92c2ea733735b876
Paul Jenkins <paul@turnpiece.com>
2014-07-24 18:14:04 +0100

    invoice formatting pretty much done

--- wp-content/plugins/bb-agency/Classes/invoice.pdf.php
+++ wp-content/plugins/bb-agency/Classes/invoice.pdf.php
@@ -12,6 +12,7 @@
         const FONT2 = 'Arial';
         const FONT_SIZE_MAIN = 12;
         const FONT_SIZE_SMALL = 8;
+        const FONT_SIZE_LARGE = 16;
 
         const V_SPACE = 5;
 
@@ -43,37 +44,51 @@
             $this->Image($bb, self::LEFT, 10, 35, null, null, 'http://'.self::BB_URL);
             $this->Image($kw, self::RIGHT, 15, 50, null, null, 'http://'.self::KW_URL);
 
-            //$this->SetY(50);
+            $this->v_space(6);
 
-            $this->Ln(30);
+            $this->link_font();
+
+            $this->SetX(self::LEFT);
+            $this->Write(5, self::BB_URL, 'http://'.self::BB_URL);
+
+            $this->SetX(self::RIGHT + 5);
+            $this->Write(5, self::KW_URL, 'http://'.self::KW_URL);
+
+            $this->v_space(2);
 
             $this->SetX(self::RIGHT);
             $this->SetTextColor(204, 102, 102);
-            $this->standard_font(true);
+            $this->large_font();
             $this->MultiCell(
-                self::HALF_W, 
-                8, 
-                'INVOICE NO: ' . $this->ficonv('InvoiceNumber') . 'Please quote in all payments'
+                40, 
+                self::V_SPACE, 
+                'INVOICE NO: ' . $this->ficonv('InvoiceNumber') . 'Please quote in all payments',
+                0,
+                'L'
             );
             $this->SetTextColor(0, 0, 0);
 
             $this->v_space(3);
 
-            $this->small_font(true);
-
             $cur_y = $this->GetY();
 
+            $this->small_font(true);
             $this->SetX(self::LEFT);
-            $this->Cell(30, self::V_SPACE, 'Accounts e-mail');
-            $this->SetX(self::LEFT + 30);
-            $this->Cell(30, self::V_SPACE, 'zandra@beautifulbumpsagency.co.uk');
+            $this->Cell(25, self::V_SPACE, 'Accounts e-mail');
+
+            $this->link_font();
+            $this->SetX(self::LEFT + 26);
+            $this->Write(self::V_SPACE, 'zandra@beautifulbumpsagency.co.uk');
 
             $this->v_space();
 
+            $this->small_font(true);
             $this->SetX(self::LEFT);
-            $this->Cell(30, self::V_SPACE, 'Accounts e-mail');
-            $this->SetX(self::LEFT + 30);
-            $this->Cell(30, self::V_SPACE, 'zandra@kiddiwinksagency.co.uk');
+            $this->Cell(25, self::V_SPACE, 'Accounts e-mail');
+
+            $this->link_font();
+            $this->SetX(self::LEFT + 26);
+            $this->Write(self::V_SPACE, 'zandra@kiddiwinksagency.co.uk');
 
             $this->v_space(2);
             
@@ -134,17 +149,18 @@
             $col_width = self::HALF_W;
             $this->debug("column width = $col_width");
 
-            $this->v_space(10);
+            $this->v_space(7);
 
             $this->SetX(self::LEFT);
             $this->standard_font(true);
-            $this->Cell(50, self::V_SPACE, $this->iconv('Services'));
+            $this->Cell(50, self::V_SPACE, $this->iconv('SERVICES'));
 
-            $this->v_space(3);
+            $this->v_space(2);
             $this->standard_font();  
-            
-            $r = 0;
+
             foreach ($this->invoice['rows'] as $row) :
+
+                $cur_y = $this->GetY();
                 
                 $row_height = 0;
 
@@ -152,12 +168,14 @@
                 $number_of_lines = ceil($total_string_width / ($col_width + 5));
 
                 $line_height = self::V_SPACE;
-                $height_of_cell = ceil($number_of_lines * $line_height) + ($line_height * 2); 
+                $height_of_cell = ceil(($number_of_lines - 1) * $line_height) + ($line_height * 2); 
 
                 if ($height_of_cell > $row_height)
                     $row_height = $height_of_cell;
 
                 $space_left = self::PAGE_HEIGHT - $this->GetY() - self::BOTTOM_MARGIN;
+
+                $this->debug("row height = $row_height");
 
                 if ($row_height >= $space_left) {   
                     // start a new page
@@ -166,19 +184,16 @@
                 }
 
                 // description
-                $this->SetX(self::LEFT);
+                $this->SetXY(self::LEFT, $cur_y);
                 $this->MultiCell($col_width, self::V_SPACE, $row[0]);
 
                 // price
-                $this->SetX(self::RIGHT);
+                $this->SetXY(self::RIGHT, $cur_y);
                 $this->Cell(30, self::V_SPACE, $this->price($row[1]), 0, 0, 'R');
                 
-                $this->Ln($row_height);
-                $cur_y = $this->GetY(); 
+                $this->Ln($row_height); 
 
             endforeach;
-            
-            $this->SetY($cur_y + self::V_SPACE);
 
             $this->standard_font(true);
             $this->SetX(self::LEFT);
@@ -208,7 +223,7 @@
             $this->Cell(self::HALF_W, 5, $this->iconv('134 Ridge Langley, South Croydon, Surrey, CR2 0AS.'));
 
             $this->SetX(self::HALF_RIGHT);
-            $this->Cell(self::HALF_W, 5, $this->iconv('Beautiful Bumps Ltd Registration No. 06320457'));
+            $this->Cell(self::HALF_W, 5, $this->iconv('Beautiful Bumps Ltd Registration No. 06320457'), 0, 0, 'R');
         }
 
         function price($amount) {
@@ -224,12 +239,20 @@
                 return $this->iconv($this->invoice[$field], $append);
         }
 
+        function large_font() {
+            $this->SetFont(self::FONT2, 'B', self::FONT_SIZE_LARGE);
+        }
+
         function standard_font($bold = false) {
             $this->SetFont(self::FONT, $bold ? 'B' : '', self::FONT_SIZE_MAIN);
         }
 
         function small_font($bold = false) {
             $this->SetFont(self::FONT2, $bold ? 'B' : '', self::FONT_SIZE_SMALL);
+        }
+
+        function link_font() {
+            $this->SetFont(self::FONT2, 'U', self::FONT_SIZE_SMALL);
         }
 
         function v_space($number = 1) {
