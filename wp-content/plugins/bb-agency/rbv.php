7888c0814c1b99d7af94218c0307abd010e698f2
Paul Jenkins <paul@turnpiece.com>
2014-08-13 20:17:53 +0100

    removed version file

--- wp-content/plugins/bb-agency/rbv.php
+++
@@ -1,7 +0,0 @@
-<?php
- header("Content-type: text/xml; charset=utf-8"); 
-  echo "<?xml version=\"1.0\"?>";
-  echo "<bb_agency_version>";
-  echo get_option("bb_agency_version");
-  echo "</bb_agency_version>";
-?>
