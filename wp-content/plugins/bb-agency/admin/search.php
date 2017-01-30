c007b438ee90d98b4c46517bcdb20e901b539a08
Paul Jenkins <paul@turnpiece.com>
2017-01-30 12:21:32 +0000

    $wpdb changes

--- wp-content/plugins/bb-agency/admin/search.php
+++ wp-content/plugins/bb-agency/admin/search.php
@@ -973,15 +973,15 @@ EOF;
                         <?php
                             //bb_custom_fields(0, $ProfileID, $ProfileGender,false);
                             $query1 = "SELECT ProfileCustomID, ProfileCustomTitle, ProfileCustomType, ProfileCustomOptions, ProfileCustomOrder, ProfileCustomView, ProfileCustomShowGender, ProfileCustomShowProfile, ProfileCustomShowSearch, ProfileCustomShowLogged, ProfileCustomShowAdmin FROM ". table_agency_customfields ." WHERE ProfileCustomView IN('0','1')  AND ProfileCustomID != 39 AND ProfileCustomID != 48 ORDER BY ProfileCustomOrder ASC";
-                            $results1 = mysql_query($query1);
-                            $count1 = mysql_num_rows($results1);
+                            $results1 = $wpdb->get_results($query1);
+                            $count1 = count($results1);
                             $pos = 0;
-                            while ($data1 = mysql_fetch_array($results1)) :
+                            foreach ($results1 as $data1) :
                                 // set data vars
-                                $id = $data1['ProfileCustomID'];
-                                $title = $data1['ProfileCustomTitle'];
-                                $type = $data1['ProfileCustomType'];
-                                $options = $data1['ProfileCustomOptions'];
+                                $id = $data1->ProfileCustomID;
+                                $title = $data1->ProfileCustomTitle;
+                                $type = $data1->ProfileCustomType;
+                                $options = $data1->ProfileCustomOptions;
                                 $field = 'ProfileCustomID'.$id;
                         ?>
                         <tr>
@@ -1275,7 +1275,7 @@ EOF;
                             </td>
                         </tr>
             
-                            <?php endwhile; //end of while ($data1
+                            <?php endforeach; //end of while ($data1
 
                             // status filter
                             ?>
