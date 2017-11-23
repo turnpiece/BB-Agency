0f57a7180dc2abf5e2b1f3bd158cc06a5eb79c99
Paul Jenkins <paul@turnpiece.com>
2017-11-23 15:11:35 +0000

    version 2.0.2

--- wp-content/plugins/bb-agency/bb-agency.php
+++ wp-content/plugins/bb-agency/bb-agency.php
@@ -5,10 +5,10 @@
   Description: Forked from RB Agency plugin and adapted for the Beautiful Bumps agency. With this plugin you can easily manage models' profiles and information.
   Author: Paul Jenkins
   Author URI: http://turnpiece.com/
-  Version: 2.0.1
+  Version: 2.0.2
 */
 
-$bb_agency_VERSION = "2.0.1"; // starter
+$bb_agency_VERSION = "2.0.2"; // starter
 
 if (!get_option("bb_agency_version")) {  
 	add_option("bb_agency_version", $bb_agency_VERSION , '', 'no');  
