8cff46ce6be888db44bf195411bda2956d9368e2
Paul Jenkins <paul@turnpiece.com>
2018-01-19 16:27:40 +0000

    relabelled fix protocol plugin

--- wp-content/plugins/tp-fix-protocol/tp-fix-protocol.php
+++ wp-content/plugins/tp-fix-protocol/tp-fix-protocol.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
-Plugin Name: Which? Fix Protocol
-Plugin URI: http://www.which.co.uk
+Plugin Name: Turnpiece Fix Protocol
+Plugin URI: http://turnpiece.com
 Description: Change http => https within posts
 Version: 0.0.1
-Author: Paul Jenkins @ Which?
+Author: Paul Jenkins @ Turnpiece
 Author URI: http://www.turnpiece.com
 License: GPLv2 or later
 */
 
 require 'class.fix-protocol.php';
 
-new Which_Fix_Protocol;
+new TP_Fix_Protocol;
